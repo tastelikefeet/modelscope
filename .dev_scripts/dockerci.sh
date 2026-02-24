e5768097f87e943c120d24b3526a2ef86117f94b
tastelikefeet <yuze.zyz@alibaba-inc.com>
2026-02-24 16:10:54 +0800

    add print

--- .dev_scripts/dockerci.sh
+++ .dev_scripts/dockerci.sh
@@ -9,6 +9,7 @@ cpu_sets_arr=($cpu_sets)
 is_get_file_lock=false
 CI_COMMAND=${CI_COMMAND:-bash .dev_scripts/ci_container_test.sh python tests/run.py --parallel 2 --run_config tests/run_config.yaml}
 echo "ci command: $CI_COMMAND"
+echo "Using docker image: $IMAGE_NAME:$IMAGE_VERSION"
 PR_CHANGED_FILES="${PR_CHANGED_FILES:-}"
 echo "PR modified files: $PR_CHANGED_FILES"
 PR_CHANGED_FILES=${PR_CHANGED_FILES//[ ]/#}
